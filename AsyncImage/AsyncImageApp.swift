9798ac0fc122f3d2ddd1cd658f36187f0543d6e1
Viacheslav Dulnev <viacheslav.dulnev@medtronic.com>
2022-01-05 18:42:15 +0200

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Viacheslav Dulnev on 05.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
